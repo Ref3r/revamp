f423034692c44fc865ab6ffe7338c425b2d685ff
sagar <sagardabas2019@gmail.com>
2025-03-05 12:20:59 +0530

    changed the sidebar for analytics-dashborad

--- src/app/analytics-dashboard/page.tsx
+++ src/app/analytics-dashboard/page.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import Analyticssidebar from "@/components/analytics-dashboard/Analytics-sidebar";
 import Header from "@/components/analytics-dashboard/Header";
 import Data from "@/components/analytics-dashboard/Data";
 import Followeranalytics from "@/components/analytics-dashboard/Followers-analytics";
 import Agesegmentation from "@/components/analytics-dashboard/Age-segmentation";
 import Leaderboard from "@/components/analytics-dashboard/Leaderboard";
+import Sidebar from "@/components/dashboard/Sidebar";
 
 const page = () => {
   return (
     <div className="bg-[#0E0E0E] h-full">
-      <Analyticssidebar />
+      <Sidebar/>
       <Header />
       <Data />
       <Followeranalytics />
